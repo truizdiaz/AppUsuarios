4add9b7430be027a45b46b143ba013f0089cb4be
truizdiaz <tomiruizdiaz@gmail.com>
2020-07-14 17:08:00 -0300

    Router Pate 2

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  irDashboard(): void {
+    // guardamos los datos en el formulario o hacemos alguna logica
+    // redireccionamos
+    this.router.navigate(['/dashboard']);
   }
 
 }
