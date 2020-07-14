5387351e208565357e99b4d55a430cd5043da2b1
truizdiaz <tomiruizdiaz@gmail.com>
2020-07-14 16:59:40 -0300

    Routing parte 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'layout', pathMatch: 'full' },
+  { path: 'layout', component: LayoutComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', redirectTo: 'layout', pathMatch: 'full' } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
