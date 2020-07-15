b09c5a163b59da9cfc26f9216c83be86a1da4cf7
truizdiaz <tomiruizdiaz@gmail.com>
2020-07-15 18:05:46 -0300

    HTML Usuario Component

--- src/app/dashboard/usuario/usuario.component.ts
+++ src/app/dashboard/usuario/usuario.component.ts
@@ -9,6 +9,12 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 })
 export class UsuarioComponent implements OnInit {
   id: number;
+  firstName: string;
+  lastName: string;
+  email: string;
+  urlImg: string;
+  phone: string;
+  gender: string;
   constructor(private aRoute: ActivatedRoute,
               private usuarioService: UsuarioService) {
     this.id = +this.aRoute.snapshot.paramMap.get('id');
@@ -21,6 +27,12 @@ export class UsuarioComponent implements OnInit {
   getUsuario(): void {
     this.usuarioService.getUsuario(this.id).subscribe(data => {
       console.log(data);
+      this.firstName = data.result.first_name;
+      this.lastName = data.result.last_name;
+      this.email = data.result.email;
+      this.phone = data.result.phone;
+      this.gender = data.result.gender;
+      this.urlImg = data.result._links.avatar.href;
     });
   }
 
